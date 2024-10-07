59690761fff48fc615da17480d76ccf6ba18d467
jhovekamp <knippjl@mail.uc.edu>
2024-10-07 00:29:25 -0400

    T4

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+import java.util.Random;  // at the top before the main
+import java.util.Scanner;
+import static java.lang.System.in;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        Random generator = new Random(); // I usually call it rnd or gen
+        int val = generator.nextInt(10) + 1;  // val is 1 to 10
+        Scanner in = new Scanner(System.in);
+        int num = 0;
+        String trash = ""; // use for bad input which will read as a String
+        boolean done = false;
+        do
+        {
+            System.out.print("Enter a number [1-10]: ");
+            if(in.hasNextInt())// is integer?
+            {
+                num = in.nextInt();
+                in.nextLine(); // clears the newline from the buffer
+                done = true; // we got a valid number so we can end the loop
+            }
+            else
+            {
+                // Not a int so use nextLine() instead to read a String
+                trash = in.nextLine();
+                System.out.println("\nYou entered: " + trash);
+                System.out.println("You have to enter a valid number.");
+            }
+        }while(!done); // initially done is false so !done i.e. NOT FALSE, is true
+        // when done is true !done i.e. NOT TRUE is false
+        System.out.println("\nThe random number is: " + val);
+        System.out.println("\nYou entered: " + num);
+        if (num > val)
+        {
+            done = true;
+            System.out.println("\nYour number is high!");
+        }
+        if (num < val)
+        {
+            done = true;
+            System.out.println("\nYour number is low!");
+        }
+        if (num == val)
+        {
+            done = true;
+            System.out.println("\nYour number is on the money!");
+        }
+    }
+}
